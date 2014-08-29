b7803a16f8669ff73765bf1755fc2e767f0e3742
Patrick Landreman <patrick.landreman@gmail.com>
2014-08-28 20:04:46 -0700

    add dimension to support multiple thicknesses

--- EndFacet.py
+++ EndFacet.py
@@ -23,8 +23,7 @@ np.set_printoptions(linewidth=150, precision=8)
 pi = sp.pi
 sqrt = sp.emath.sqrt
 mu = 4*pi * 1e-7
-# c  = 299792458
-c = 3e8
+c  = 299792458
 
 # units
 cm = 1e-2
@@ -71,7 +70,18 @@ def beta_marcuse(n,d,wl=1.,pol='TM',Nmodes=1,plot=False):
 	# between the two nearest points in the array
 
 	b = np.linspace(0,n*k - (1e-15),10000)
+
+	# if type(d) not in [int, float] and len(d) > 1:
+	# 	d = d.reshape(len(d),1)
+	# 	b = np.tile(b,(len(d),1))
+
 	diff = np.diff(np.sign(trans(b)))
+
+	# plt.plot(b[0,:],trans(b)[0,:])
+	# plt.plot(b[0,:],np.sign(trans(b))[0,:])
+	# plt.ylim(-2,2)
+	# plt.show()
+	# exit()
 
 	for i,idx in enumerate(np.nonzero(diff)[0][::-1]):
 
@@ -88,7 +98,7 @@ def beta_marcuse(n,d,wl=1.,pol='TM',Nmodes=1,plot=False):
 		plt.ylim(-10,10)
 		plt.show()
 
-	return Bs
+	return np.array(Bs)
 
 def test_beta_marcuse():
 
@@ -172,12 +182,15 @@ def pythonic_main():
 	# Define Key Simulation Parameters
 
 	n = sqrt(20)
-	d = 1 * cm
-	kd = np.array([0.209,0.418,0.628,0.837,1.04,1.25])
+	wl = 10.
+	# d = 1 * cm
+	# kd = np.array([0.209,0.628])
+	# kd = np.array([0.209,0.418,0.628,0.837,1.04,1.25])
+	kd = np.linspace(1e-15,1.5,100)
 	pol = 'TE'
 
 	p_max = 20 			# max transverse wavevector to use for indefinite integrals; multiples of k
-	p_res = 2e3     # number of steps to use in integration
+	p_res = 1e3     # number of steps to use in integration
 
 	imax = 4 				# max number of iterations to run
 
@@ -186,11 +199,12 @@ def pythonic_main():
 	'''
 
 	# constants
-	k = kd/d
-	wl = 2*pi/k
+	k = 2*pi/wl
+	d = kd/k
 	w = c*k
 	eps = n**2
 
+	Nds = len(d)
 	
 	# # Debugger for mode solver
 	# wl = 1.0
@@ -203,15 +217,21 @@ def pythonic_main():
 	
 
 	# wavevectors
-	N = numModes(n,1,2*d/wl)
-	Bm = beta_marcuse(n,d,wl=wl,Nmodes=N, pol=pol)			# Propagation constants of waveguide modes
-	Bo = Bm[0,:]
 
-	print Bm.shape()
-	exit()
+	Ns = numModes(n,1,2*d/wl)
+	N = np.amax(Ns)
+	
+	Bo = np.zeros(len(d))
+	Bm = np.zeros((np.amax(N),len(d)))
 
-	p = np.linspace(1e-15,p_max*k,p_res)					# transverse wavevectors in air (independent variable for algorithm)
-	dp = p[1]-p[0]
+	for j,dj in enumerate(d):
+		Bm[:,j] = beta_marcuse(n,dj,wl=wl,Nmodes=N, pol=pol)			# Propagation constants of waveguide modes
+		Bo[j] = Bm[0,j]
+
+	p = np.tile(np.linspace(1e-15,p_max*k,p_res), (len(d),1))					# transverse wavevectors in air (independent variable for algorithm)
+	p = p.transpose()
+
+	dp = p[1,0]-p[0,0]
 	
 	Bc = sqrt(       k**2 - p**2) 						# propagation constants in air for continuum modes
 	o  = sqrt(n**2 * k**2 - Bc**2)						# transverse wavevectors in high-index region for continuum modes
@@ -233,10 +253,7 @@ def pythonic_main():
 
 	Am = sqrt(2*w*mu*P / (Bm*d + Bm/gm))
 
-	# Bt = sqrt(2*w*mu*P / (pi*Bc)) # might need to make these |Bc| instead of Bc
-	# Br = sqrt(2*p**2*w*mu*P / (pi*Bc*(p**2*cos(o*d)**2 + o**2*sin(o*d)**2)))
-
-	Bt = sqrt(2*w*mu*P / (pi*abs(Bc))) # might need to make these |Bc| instead of Bc
+	Bt = sqrt(2*w*mu*P / (pi*abs(Bc))) 
 	Br = sqrt(2*p**2*w*mu*P / (pi*abs(Bc)*(p**2*cos(o*d)**2 + o**2*sin(o*d)**2)))
 	
 	Dr = 1/2. * exp(-1j*p*d) * (cos(o*d) + 1j*o/p * sin(o*d))
@@ -254,21 +271,24 @@ def pythonic_main():
 
 	# Overlap Integral Solutions, Gm(p) and F(p',p) = F(p1,p2)
 
-	G = np.zeros((N,p_res),dtype = 'complex')
+	G = np.zeros((N,p_res,Nds),dtype = 'complex')
 
 	for i in range(N):
-		G[i,:] = 2 * k**2 * (eps-1) * Am[i] * Bt * cos(Km[i]*d) * (gm[i]*cos(p*d) - p*sin(p*d)) / ((Km[i]**2 - p**2)*(gm[i]**2 + p**2))
+		G[i,:,:] = 2 * k**2 * (eps-1) * Am[i] * Bt * cos(Km[i]*d) * (gm[i]*cos(p*d) - p*sin(p*d)) / ((Km[i]**2 - p**2)*(gm[i]**2 + p**2))
 
-	p1,p2 = np.meshgrid(p,p)			# Move to 2D arrays
-	Br1 = np.tile(Br,(p_res,1))
-	Bt1 = np.tile(Bt,(p_res,1)); Bt2 = Bt1.transpose()
-	o1  = np.tile(o ,(p_res,1))
-	Bc1 = np.tile(Bc,(p_res,1)); Bc2 = Bc1.transpose()
+	# Convert to multi-dimensional arrays to work with functions of p',p. When transposing, 
+	# the index refering to d is kept as the final index
+
+	p1  = np.tile(p, (p_res,1,1)); p2 = p1.transpose(1,0,2)
+	Br1 = np.tile(Br,(p_res,1,1))
+	Bt1 = np.tile(Bt,(p_res,1,1)); Bt2 = Bt1.transpose(1,0,2)
+	o1  = np.tile(o ,(p_res,1,1))
+	Bc1 = np.tile(Bc,(p_res,1,1)); Bc2 = Bc1.transpose(1,0,2)
 	
-	D1  = np.tile(Dr,(p_res,1))
+	D1  = np.tile(Dr,(p_res,1,1))
 	Dstar  = np.conjugate(D1)
 
-	od = o1*d; pd = p2*d
+	od = o1*d; pd = p2*d.transpose()
 
 	# This method for F is based on a more direct solution of the field overlap integral, with less
 	# subsequent algebra:
@@ -276,14 +296,16 @@ def pythonic_main():
 	# 								 2*(D1 * (exp(1j*p1*d) * (p2*sin(pd)+1j*p1*cos(pd))) - 1j*p1 ).real / (p1**2-p2**2) )
 	
 	# Handle Cauchy singularities
-	cauchy = pi * Bt1.transpose() * Br1 * (D1 + Dstar)
-	idx = np.where(1 - np.isfinite(F))
+	# cauchy = pi * Bt2 * Br1 * (D1 + Dstar)
+	# idx = np.where(1 - np.isfinite(F))
 	# F[idx] = 0
 	# F[idx] = cauchy[idx]
 
 	# Gelin's definition of F, with the sin arguments corrected:
-	F = np.eye(p_res) * Bt1.transpose() * Br1 * (D1 + Dstar) - \
-			np.nan_to_num(k**2 * (eps-1) * Bt2 * Br1 * (sin((o1+p2)*d)/(o1+p2) + sin((o1-p2)*d)/(o1-p2)) / (p1**2-p2**2)) * (1-np.eye(p_res))
+	I = np.tile(np.eye(p_res), (Nds,1,1)).transpose(1,2,0)
+	F = I * Bt2 * Br1 * (D1 + Dstar) - \
+			np.nan_to_num(k**2 * (eps-1) * Bt2 * Br1 * (sin((o1+p2)*d)/(o1+p2) + sin((o1-p2)*d)/(o1-p2)) / (p1**2-p2**2)) * (1-I)
+
 
 
 
@@ -293,15 +315,15 @@ def pythonic_main():
 	'''
 
 	# Initialize coefficients to zero
-	am = np.zeros(N)
-	qr = np.zeros(p_res)	
+	am = np.zeros((N,Nds))
+	qr = np.zeros((p_res,Nds))	
 
 	for i in range(imax):
 
 		print '\nComputing iteration %u of %u' % (i+1,imax)
 
 		# Qt
-		qr1 = np.tile(qr,(p_res,1))
+		qr1 = np.tile(qr,(p_res,1,1))
 		integral = np.trapz(qr1 * (Bo-Bc1) * F, dx=dp, axis=1)
 		# integral = np.sum(qr1 * (Bo-Bc1) * F, axis=1) * dp
 
@@ -312,15 +334,14 @@ def pythonic_main():
 		# an
 		am_prev = am
 		am = np.array(
-			[ (1/(4*w*mu*P) * np.sum(qt * (Bm[n]-Bc) * G[n,:]) * dp) for n in range(N) ]
+			[ (1/(4*w*mu*P) * np.sum(qt * (Bm[n]-Bc) * G[n,:], axis=0) * dp) for n in range(N) ]
 			)
-		print am
 		print 'Delta ao: ', abs(am_prev-am)
 
 
 		#Qr
-		qt1 = np.tile(qt,(p_res,1))
-		integral = np.trapz(qt1 * (Bc2-Bc1) * F.transpose(), dx=dp, axis=1)
+		qt1 = np.tile(qt,(p_res,1,1))
+		integral = np.trapz(qt1 * (Bc2-Bc1) * F.transpose(1,0,2), dx=dp, axis=1)
 		# integral = np.sum(qt1 * (Bc2-Bc1) * F.transpose(), axis=1) * dp
 		
 		qr = 1/(4*w*mu*P) * (abs(Bc)/Bc) * integral
@@ -329,20 +350,28 @@ def pythonic_main():
 		Test Power Conservation
 		'''
 
-		error = (1+am[0]) * (1-am[0].conjugate()) - np.sum(abs(am[1:])**2) - np.sum((abs(qt)**2 + abs(qr)**2) * np.conjugate(Bc) / abs(Bc)) * dp
+		error = (1+am[0,:]) * (1-am[0,:].conjugate()) - np.sum(abs(am[1:,:])**2, axis=0) - np.sum((abs(qt)**2 + abs(qr)**2) * np.conjugate(Bc) / abs(Bc), axis=0) * dp
 		print 'Power Conservation: ', abs(error.real)
 
 		'''
 		Test Gelin, eq. 14
 		'''
 
-		print 1/(4*w*mu*P) * np.trapz(qt * (Bm[0]+Bc) * G[0,:], dx=dp)
+		print 'Gelin Eq. 14: ', 1/(4*w*mu*P) * np.trapz(qt * (Bm[0]+Bc) * G[0,:], dx=dp, axis=0)
+
+	'''
+	Print Stats
+	'''
+
+	print '\n----Results Summary----\n'
+
+	print 'Max Power Conservation Error:', np.amax(abs(error.real))
 
 	'''
 	Plot results
 	'''
 
-	fig, ax = plt.subplots(2,figsize=(7,5),sharex=True)
+	fig, ax = plt.subplots(3,figsize=(7,8))
 
 	ax[0].plot(p/k,qt.real,'r')
 	ax[0].plot(p/k,qt.imag,'r:')
@@ -352,8 +381,14 @@ def pythonic_main():
 	ax[0].axhline(0,color='k',ls=':')
 	ax[1].axhline(0,color='k',ls=':')
 
-	ax[0].set_xlim(0,150/k)
-	# ax[0].set_xlim(0,1,5)
+	# ax[0].set_xlim(0,10)
+	# ax[0].set_ylim(0,1.5)
+	# ax[1].set_xlim(0,10)
+	# ax[1].set_ylim(0,1.5)
+
+	ax[2].plot(kd,am[0,:],'g')
+	ax[2].set_ylim(0,1)
+
 	# fig2, ax2 = plt.subplots(1,figsize=(7,5))
 	# ax2.plot(p/k,Bc.real,'g')
 	# ax2.plot(p/k,Bc.imag,'g:')
