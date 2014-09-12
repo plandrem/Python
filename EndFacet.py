0b2e7e0c88b00c69f529d18385ddc22a67a0d592
Patrick Landreman <patrick.landreman@gmail.com>
2014-09-12 00:18:00 -0700

    add high res zone of p values below 2k; TM converging; TE and TM values do not match table in Gelin paper

--- EndFacet.py
+++ EndFacet.py
@@ -25,7 +25,7 @@ eo = 8.85e-12
 c  = 299792458
 
 # mu = 1.
-eo = 1.
+# eo = 1.
 # c  = 1.
 
 # units
@@ -139,7 +139,7 @@ def test_beta_marcuse():
 def numModes(ncore,nclad,kd):
 	return np.ceil(sqrt(ncore**2 - nclad**2)*kd/pi).astype(int)
 
-def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,convergence_threshold=1e-5):
+def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,convergence_threshold=1e-5,debug=False):
 	'''
 
 	Solve for amplitudes of all scattered modes resulting from a guided mode incident on the 
@@ -188,6 +188,8 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 	eps = n**2
 
 	Nds = len(d)
+
+	TM = (pol == 'TM')
 	
 	# # Debugger for mode solver
 	# wl = 1.0
@@ -196,12 +198,17 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 	# d = kd/k	
 	# print beta_marcuse(1.432,d,wl=wl,pol='TE',plot=True) * d
 	# print 'Should be 1.21972'
+
+	# print beta_marcuse(1.432,d,wl=wl,pol='TM',plot=True) * d
+	# print 'Should be 1.12809'
+
 	# exit()
 	
 
 	# wavevectors
 
 	Ns = numModes(n,1,kd)
+	# N = 1
 	N = np.amax(Ns)
 	print 'Number of Modes Detected:', N
 
@@ -209,20 +216,19 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 	if incident_mode+1 > N:
 		print 'Source Mode below cutoff.'
 		return np.nan * np.ones(N)
-
-	# dd = d*n/wl
-	# print Ns
-	# print ds.numModes(n,1,dd)
-	# exit()
 	
 	Bo = np.zeros(len(d))
+	# Bm = np.ones((np.amax(N),len(d)))
 	Bm = np.zeros((np.amax(N),len(d)))
 
 	for j,dj in enumerate(d):
 		Bm[:,j] = beta_marcuse(n,dj,wl=wl,Nmodes=N, pol=pol, plot=False)			# Propagation constants of waveguide modes
 		Bo[j] = Bm[incident_mode,j]
 
-	p = np.tile(np.linspace(1e-15,p_max*k,p_res), (len(d),1))					# transverse wavevectors in air (independent variable for algorithm)
+	p = np.hstack((np.linspace(1e-15,2*k,p_res),np.linspace(2*k + p_max*k/p_res, p_max*k, p_res)))
+
+	p = np.tile(p, (len(d),1))					# transverse wavevectors in air (independent variable for algorithm)
+	# p = np.tile(np.linspace(1e-15,p_max*k,p_res), (len(d),1))					# transverse wavevectors in air (independent variable for algorithm)
 	p = p.transpose()
 
 	dp = p[1,0]-p[0,0]
@@ -238,7 +244,7 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 	gm = gm.real - 1j*gm.imag
 	Km = Km.real - 1j*Km.imag
 
-	
+
 
 
 
@@ -256,67 +262,113 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 
 	else:
 
-		Am = sqrt(gm/Bm * 2*w*eo*n**2*P / ( n**2*k**2/(Bm**2 + n**2*gm**2) + gm*d))
+		Am = sqrt(gm/Bm * 2*w*eo*n**2*P / ( n**2 * k**2/(Bm**2 + n**2*gm**2) + gm*d))
 
 		Bt = sqrt(2*w*eo*P / (pi*abs(Bc))) 
 		Br = sqrt(2*p**2*w*eo*P*n**2 / (pi*abs(Bc)*(n**2 * p**2 * cos(o*d)**2 + o**2/n**2 * sin(o*d)**2)))
 
 		Dr = 1/2. * exp(-1j*p*d) * (cos(o*d) + 1j*o/(n**2 * p) * sin(o*d))
 
+	# Plot Amplitudes
+	if debug:
+		plt.figure()
+		plt.plot(p/k,Bt,'b')
+		plt.plot(p/k,Br,'r')
+		print '\nMode Amplitudes:', Am
+		# print sqrt(P*w*eo/(Bm * (1/(eps*4*Km) * (2*Km*d + sin(2*Km*d)) + (cos(Km*d)**2)/(2*gm))))
+		# print sqrt(P*w*eo/(Bm * (1/(4*Km) * (2*Km*d + sin(2*Km*d)) + (cos(Km*d)**2)/(2*gm))))
 
 
 	# Overlap Integral Solutions, Gm(p) and F(p',p) = F(p1,p2)
 
-	G = np.zeros((N,p_res,Nds),dtype = 'complex')
-	V = np.zeros((N,p_res,Nds),dtype = 'complex')
+	G = np.zeros((N,len(p),Nds),dtype = 'complex')
+	V = np.zeros((N,len(p),Nds),dtype = 'complex')
+	kp = np.zeros((N,len(p),Nds),dtype = 'complex')
 
 	for i in range(N):
 		G[i,:,:] = 2 * k**2 * (eps-1) * Am[i] * Bt * cos(Km[i]*d) * (gm[i]*cos(p*d) - p*sin(p*d)) / ((Km[i]**2 - p**2)*(gm[i]**2 + p**2))
 
 		if pol == 'TM':
-			V[i,:,:] = Am[i] * Bt * cos(Km[i]*d) * (gm[i]*cos(p*d)*(gm[i]**2 + Km[i]**2) - p*sin(p*d)*((gm[i]**2 + p**2)/n**2 + Km[i]**2 - p**2)) \
-								 / ((Km[i]**2 - p**2)*(gm[i]**2 + p**2))
+			# V[i,:,:] = Am[i] * Bt * cos(Km[i]*d) * (gm[i]*cos(p*d)*(gm[i]**2 + Km[i]**2) - p*sin(p*d)*((gm[i]**2 + p**2) + Km[i]**2 - p**2)) \
+			# 					 / ((Km[i]**2 - p**2)*(gm[i]**2 + p**2))
+			# V[i,:,:] = Am[i] * Bt * cos(Km[i]*d) * (gm[i]*cos(p*d)*(gm[i]**2 + Km[i]**2) - p*sin(p*d)*((gm[i]**2 + p**2)/n**2 + Km[i]**2 - p**2)) \
+			# 					 / ((Km[i]**2 - p**2)*(gm[i]**2 + p**2))
+
+			V[i,:,:]  = Am[i] * Bt * cos(Km[i]*d) * ((Km[i]*sin(p*d) - p*cos(p*d)*tan(Km[i]*d))/(eps*(Km[i]**2 - p**2)) \
+				           - (p*sin(p*d) - gm[i]*cos(p*d))/(gm[i]**2 + p**2))
+
+			kp[i,:,:] = Am[i] * Bt * cos(Km[i]*d) * ((Km[i]*sin(p*d) - p*cos(p*d)*tan(Km[i]*d))/(Km[i]**2 - p**2) \
+				           - (p*sin(p*d) - gm[i]*cos(p*d))/(gm[i]**2 + p**2))
 
 
 	# Convert to multi-dimensional arrays to work with functions of p',p. When transposing, 
 	# the index refering to d is kept as the final index
 
-	p1  = np.tile(p, (p_res,1,1)); p2 = p1.transpose(1,0,2)
-	Br1 = np.tile(Br,(p_res,1,1))
-	Bt1 = np.tile(Bt,(p_res,1,1)); Bt2 = Bt1.transpose(1,0,2)
-	o1  = np.tile(o ,(p_res,1,1))
-	Bc1 = np.tile(Bc,(p_res,1,1)); Bc2 = Bc1.transpose(1,0,2)
+	p1  = np.tile(p, (len(p),1,1)); p2 = p1.transpose(1,0,2)
+	Br1 = np.tile(Br,(len(p),1,1))
+	Bt1 = np.tile(Bt,(len(p),1,1)); Bt2 = Bt1.transpose(1,0,2)
+	o1  = np.tile(o ,(len(p),1,1))
+	Bc1 = np.tile(Bc,(len(p),1,1)); Bc2 = Bc1.transpose(1,0,2)
 	
-	D1  = np.tile(Dr,(p_res,1,1))
+	D1  = np.tile(Dr,(len(p),1,1))
 	Dstar  = np.conjugate(D1)
 
 	od = o1*d; pd = p2*d.transpose()
 
 	# This method for F is based on a more direct solution of the field overlap integral, with less
 	# subsequent algebra:
-	# F  = 2*Br1*Bt2 * ((o1*sin(od)*cos(pd) - p2*cos(od)*sin(pd))/(o1**2-p2**2) + \
+	# F = 2*Br1*Bt2 * ((o1*sin(od)*cos(pd) - p2*cos(od)*sin(pd))/(o1**2-p2**2) + \
 	# 								 2*(D1 * (exp(1j*p1*d) * (p2*sin(pd)+1j*p1*cos(pd))) - 1j*p1 ).real / (p1**2-p2**2) )
 	
-	# Handle Cauchy singularities
+	# # # Handle Cauchy singularities
 	# cauchy = pi * Bt2 * Br1 * (D1 + Dstar)
 	# idx = np.where(1 - np.isfinite(F))
 	# F[idx] = 0
-	# F[idx] = cauchy[idx]
+	# # F[idx] = cauchy[idx]
 
 	# Gelin's definition of F, with the sin arguments corrected:
-	I = np.tile(np.eye(p_res), (Nds,1,1)).transpose(1,2,0)
-	F = I * pi * Bt2 * Br1 * (D1 + Dstar) - \
+	I = np.tile(np.eye(len(p)), (Nds,1,1)).transpose(1,2,0)
+	F = 0*I * pi * Bt2 * Br1 * (D1 + Dstar) - \
 			np.nan_to_num(k**2 * (eps-1) * Bt2 * Br1 * (sin((o1+p2)*d)/(o1+p2) + sin((o1-p2)*d)/(o1-p2)) / (p1**2-p2**2)) * (1-I)
 
 	if pol == 'TM':
 		f = Br1*Bt2 * ((o1*sin(od)*cos(pd) - p2*cos(od)*sin(pd))/(n**2 * (o1**2-p2**2)) + \
 										 2*(D1 * (exp(1j*p1*d) * (p2*sin(pd)+1j*p1*cos(pd))) - 1j*p1 ).real / (p1**2-p2**2) )
-		
+
+		z = Br1*Bt2 * ((o1*sin(od)*cos(pd) - p2*cos(od)*sin(pd))/        (o1**2-p2**2)  + \
+										 2*(D1 * (exp(1j*p1*d) * (p2*sin(pd)+1j*p1*cos(pd))) - 1j*p1 ).real / (p1**2-p2**2) )
+
+
 		# Handle Cauchy singularities
 		cauchy = pi * Bt2 * Br1 * (D1 + Dstar)
 		idx = np.where(1 - np.isfinite(f))
-		# f[idx] = 0
-		f[idx] = cauchy[idx]
+		f[idx] = 0
+		# f[idx] = cauchy[idx]
+
+		idx = np.where(1 - np.isfinite(z))
+		z[idx] = 0
+		
+
+	if debug:
+
+		plt.figure()
+		# plt.plot(p/k,G[0],'g')
+		if TM: 
+			plt.plot(p/k,kp[0],'g')
+			plt.plot(p/k,V[0],'m')
+
+		print p[100]
+
+		# plt.figure()
+		# plt.plot(p/k,F[:,100,0],'c')
+		if TM: 
+			plt.plot(p/k,f[:,100,0],'y')
+			plt.plot(p/k,z[:,100,0],'c')
+
+		plt.show()
+		exit()
+		
+
 
 
 
@@ -329,7 +381,7 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 
 	# Initialize coefficients to zero
 	am = np.zeros((N,Nds))
-	qr = np.zeros((p_res,Nds))	
+	qr = np.zeros((len(p),Nds))	
 
 	# Remove nan instances for iteration purposes
 	nanMask = np.isnan(Bm)
@@ -341,8 +393,10 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 	if pol == 'TM':
 		vm = V[incident_mode]
 		Gm = G[incident_mode]
+		kpm = kp[incident_mode]
 
 	repeat = True; converged = False
+	delta_prev = delta_2prev = np.inf
 
 	for i in range(imax):
 
@@ -357,7 +411,7 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 
 			# Qt
 
-			qr1 = np.tile(qr,(p_res,1,1))
+			qr1 = np.tile(qr,(len(p),1,1))
 			integral = np.trapz(qr1 * (Bo-Bc1) * F, dx=dp, axis=1)
 			# integral = np.sum(qr1 * (Bo-Bc1) * F, axis=1) * dp
 
@@ -372,7 +426,7 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 				)
 
 			#Qr
-			qt1 = np.tile(qt,(p_res,1,1))
+			qt1 = np.tile(qt,(len(p),1,1))
 			integral = np.trapz(qt1 * (Bc2-Bc1) * F.transpose(1,0,2), dx=dp, axis=1)
 			# integral = np.sum(qt1 * (Bc2-Bc1) * F.transpose(), axis=1) * dp
 			
@@ -383,22 +437,22 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 
 		else: #TM
 
-			qr1 = np.tile(qr,(p_res,1,1))
-			integral = np.trapz(qr1 * (Bc1*Gm*f - Bo*vm*F), dx=dp, axis=1)
+			qr1 = np.tile(qr,(len(p),1,1))
+			integral = np.trapz(qr1 * (Bo*vm*z - Bc1*kpm*f), dx=dp, axis=1)
 
-			sigma = np.sum([(Bm[n]*V[n]*Gm - Bo*vm*G[n]) * am[n] for n in range(N)], axis=0)
+			sigma = np.sum([(Bo*vm*kp[n] - Bm[n]*V[n]*kpm) * am[n] for n in range(N)], axis=0)
 
-			qt = 1/(2*w*eo*P) * abs(Bc) / (Bo*vm+Bc*Gm/2.) * (2*Bo*vm*Gm + integral + sigma)
+			qt = 1/(w*eo*P) * abs(Bc) / (Bo*vm+Bc*kpm) * (2*Bo*vm*kpm + integral + sigma)
 
 			# an
 			am_prev = am
 			am = np.array(
-				[ (1/(2*w*eo*P) * np.trapz(qt * (Bc*G[n]/2.-Bm[n]*V[n]), dx=dp, axis=0)) for n in range(N) ]
+				[ (1/(2*w*eo*P) * np.trapz(qt * (Bm[n]*V[n] - Bc*kp[n]), dx=dp, axis=0)) for n in range(N) ]
 				)
 
 			#Qr
-			qt1 = np.tile(qt,(p_res,1,1))
-			integral = np.trapz(qt1 * (Bc1 * F.transpose(1,0,2)/2. - Bc2 * f.transpose(1,0,2)), dx=dp, axis=1)
+			qt1 = np.tile(qt,(len(p),1,1))
+			integral = np.trapz(qt1 * (Bc2 * f.transpose(1,0,2) - Bc1 * z.transpose(1,0,2)), dx=dp, axis=1)
 			
 			qr = 1/(2*w*eo*P) * (abs(Bc)/Bc) * integral
 
@@ -410,6 +464,14 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 		delta = abs(am_prev-am)
 		repeat = True if np.any(delta > convergence_threshold) else False
 		print 'Delta ao:', np.amax(delta)
+		
+		# if difference in am has been rising for 2 iterations, value is diverging. Bail.
+
+		if np.amax(delta) > delta_prev and delta_prev > delta_2prev: break
+
+		delta_2prev = delta_prev
+		delta_prev = np.amax(delta)
+
 
 
 	'''
@@ -445,15 +507,19 @@ def Reflection(kd,n,incident_mode=0,pol='TE',p_max=20,p_res=1e3,imax=100,converg
 
 	# scattering coefficients
 
-	# fig, ax = plt.subplots(2,figsize=(7,5))
+	if debug:
+		print '\n Guided Mode Coefficients:'
+		fig, ax = plt.subplots(2,figsize=(7,5))
 
-	# ax[0].plot(p/k,qt.real,'r')
-	# ax[0].plot(p/k,qt.imag,'r:')
-	# ax[1].plot(p/k,qr.real,'b')
-	# ax[1].plot(p/k,qr.imag,'b:')
+		ax[0].plot(p/k,qt.real,'r')
+		ax[0].plot(p/k,qt.imag,'r:')
+		ax[1].plot(p/k,qr.real,'b')
+		ax[1].plot(p/k,qr.imag,'b:')
 
-	# ax[0].axhline(0,color='k',ls=':')
-	# ax[1].axhline(0,color='k',ls=':')
+		ax[0].axhline(0,color='k',ls=':')
+		ax[1].axhline(0,color='k',ls=':')
+
+		print am
 
 	# ax[0].set_xlim(0,10)
 	# ax[0].set_ylim(0,1.5)
@@ -484,31 +550,194 @@ def main():
 
 	# Define Key Simulation Parameters
 
+	# n = sqrt(1)
 	n = sqrt(20)
-	kds = np.array([.5])
+	# kds = np.array([0.25])
 	# kd = np.array([1.375]) # Diverges?!
-	# kd = np.array([0.209,0.418,0.628,0.837,1.04,1.25])
+	kds = np.array([0.209,0.418,0.628,0.837,1.04,1.25]) # TE Reference values
+	# kds = np.array([0.314,0.418,0.628,0.837,1.04,1.25]) # TM Reference Values
 	# kds = np.linspace(1e-9,2.4,50)
+	# kds = np.linspace(0.1,0.5,50)
 	# kds = np.linspace(0.5,1.5,50)
 
 	res = 0.5e3
 	incident_mode = 0
-	# pol='TE'
-	pol='TM'
+	pol='TE'
 
-	ams = putil.stackPoints([Reflection(kd,n,pol=pol,incident_mode=incident_mode,p_res=res) for kd in kds])
+	imax = 100
+	p_max = 100
 
-	fig, ax = plt.subplots(2,figsize=(7,5))
-	[ax[0].plot(kds, abs(am)     , color=colors[i]) for i,am in enumerate(ams)]
-	[ax[1].plot(kds, np.angle(am), color=colors[i]) for i,am in enumerate(ams)]
+	ams = putil.stackPoints([
+		Reflection(kd,n,
+			pol=pol,
+			incident_mode=incident_mode,
+			p_res=res,
+			imax=imax,
+			p_max=p_max,
+			debug=False
+			) for kd in kds])
+
+	# fig, ax = plt.subplots(2,figsize=(7,5))
+	# [ax[0].plot(kds, abs(am)     , color=colors[i]) for i,am in enumerate(ams)]
+	# [ax[1].plot(kds, np.angle(am), color=colors[i]) for i,am in enumerate(ams)]
+	# plt.show()
+	print ams
+	print
+	print abs(ams)**2
+
+def test_v():
+
+	kd = 1.
+
+	n = sqrt(20)
+
+	wl = 10. # Set to 10 to match Gelin values for qt
+	k = 2*pi/wl
+	d = kd/k
+	w = c*k
+	eps = n**2
+
+	k = 2*pi/wl
+	d = 1.
+
+	P = 1
+
+	Bm = beta_marcuse(n,d,wl=wl, Nmodes=1, pol='TM', plot=False)			# Propagation constants of waveguide Modes
+
+	p_max = 20
+	p_res = 300
+	ps = np.linspace(1e-15,p_max*k,p_res)
+
+	I = np.zeros(p_res)
+
+	x = np.linspace(0,10*d,10000)
+	dx = x[1]-x[0]
+
+
+	for i,p in enumerate(ps):
+		# print i
+
+		Bc = sqrt(       k**2 - p**2) 						# propagation constants in air for continuum modes
+		o  = sqrt(n**2 * k**2 - Bc**2)						# transverse wavevectors in high-index region for continuum modes
+		
+		gm  = sqrt(      -k**2 + Bm**2)						# transverse wavevectors in air for guided modes
+		Km  = sqrt(n**2 * k**2 - Bm**2)						# transverse wavevectors in high-index region for guided modes
+
+		A = sqrt(gm/Bm * 2*P*w*eo*eps / ((n*k)**2/(Bm**2 + (n*gm)**2) + gm*d))
+
+		Bt = sqrt(2*w*eo*P/(pi*abs(Bc)))
+
+		Br = p * sqrt(2*w*eo*eps*P / (pi * Bm * (eps * p**2 * cos(o*d)**2 + o**2/eps * sin(o*d)**2)))
+
+		D = 1/2. * (cos(o*d) + 1j * o/(p*n**2)*sin(o*d)) * exp(-1j*p*d)
+
+
+		Hm = A * cos(Km*x) * (x < d) + A * exp(gm*d) * cos(Km*d) * exp(-gm*x) * (x >= d)
+
+		Hr = Br * cos(o*x) * (x < d) + 2 * (Br * D * exp(1j*p*x)).real * (x >= d)
+
+		Ht = Bt * cos(p*x)
+
+
+
+		# perform overlap
+
+		integrand = (Hm * Ht.conj())/n**2 * (x <= d) + (Hm * Ht.conj()) * (x >= d)
+		I[i] = np.trapz(integrand, dx=dx)
+
+		# plt.plot(Hm.real,x/d,'r')
+		# plt.plot(Hp.real,x/d,'b')
+		# plt.plot(integrand * A/np.amax(integrand),x/d,'g')
+		# plt.axhline(1,c='k',ls=':')
+		# plt.axvline(0,c='k',ls=':')
+		# plt.show()
+
+	plt.plot(ps/k,I,'ro')
 	plt.show()
 
+def test_f():
+
+	kd = 1.
+
+	n = sqrt(20)
+
+	wl = 10. # Set to 10 to match Gelin values for qt
+	k = 2*pi/wl
+	d = kd/k
+	w = c*k
+	eps = n**2
+
+	k = 2*pi/wl
+	d = 1.
+
+	P = 1
+
+	Bm = beta_marcuse(n,d,wl=wl, Nmodes=1, pol='TM', plot=False)			# Propagation constants of waveguide Modes
+
+	p_max = 4
+	p_res = 600
+	ps = np.linspace(1e-15,p_max*k,p_res)
+
+	p2 = 0.62863285
+
+	xres = d/100.
+	xmax = np.linspace(300,302,2)
+
+	I = np.zeros((p_res,len(xmax)),dtype=complex)
+
+	for j,xm in enumerate(xmax):
+		x = np.arange(0,xm*d,xres)
+		dx = x[1]-x[0]
+
+		print j
+
+		for i,p in enumerate(ps):
+			# print i
+
+			Bc = sqrt(       k**2 - p**2) 						# propagation constants in air for continuum modes
+			o  = sqrt(n**2 * k**2 - Bc**2)						# transverse wavevectors in high-index region for continuum modes
+			
+			Bc2 = sqrt(       k**2 - p2**2) 						# propagation constants in air for continuum modes
+			o2  = sqrt(n**2 * k**2 - Bc2**2)						# transverse wavevectors in high-index region for continuum modes
+			
+			gm  = sqrt(      -k**2 + Bm**2)						# transverse wavevectors in air for guided modes
+			Km  = sqrt(n**2 * k**2 - Bm**2)						# transverse wavevectors in high-index region for guided modes
+
+			A = sqrt(gm/Bm * 2*P*w*eo*eps / ((n*k)**2/(Bm**2 + (n*gm)**2) + gm*d))
+
+			Bt = sqrt(2*w*eo*P/(pi*abs(Bc2)))
+
+			Br = p * sqrt(2*w*eo*eps*P / (pi * Bm * (eps * p**2 * cos(o*d)**2 + o**2/eps * sin(o*d)**2)))
+
+			D = 1/2. * (cos(o*d) + 1j * o/(p*n**2)*sin(o*d)) * exp(-1j*p*d)
 
 
+			Hr = Br * cos(o*x) * (x < d) + 2 * (Br * D * exp(1j*p*x)).real * (x >= d)
+
+			Ht = Bt * cos(p2*x)
+
+
+
+			# perform overlap
+
+			integrand = (Hr * Ht.conj())/n**2 * (x <= d) + (Hr * Ht.conj()) * (x >= d)
+			I[i,j] = np.trapz(integrand, dx=dx)
+
+	# plt.plot(Ht.real,x/d,'r')
+	# plt.plot(Hr.real,x/d,'b')
+	# plt.plot(integrand.real * Bt/np.amax(integrand),x/d,'g')
+	# plt.axhline(1,c='k',ls=':')
+	# plt.axvline(0,c='k',ls=':')
+
+	# plt.figure()
+	# plt.imshow(I.real,aspect='auto',interpolation='nearest')
+
+	plt.plot(ps/k,I.real)
+	plt.show()
 
 
 if __name__ == '__main__':
   # test_beta()
   # test_beta_marcuse()
   main()
-  # test_stackPoints()
+  # test_f()
